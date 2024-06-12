1719d86a0f0b76f06492dd507622e74a36f231cf
optimal99 <stegano9744@gmail.com>
2024-06-13 08:58:52 +0900

    #1-2. 삼각형 판별하기
    [조건]
    - 길이가 서로 다른 A, B, C
    - 삼각형 만들 수 있으면 YES, 아니면 NO 출력
    
    [플로우]
    1. 세 변의 길이 입력 받기
    2. 가장 긴 변 찾기
    3. 가장 긴 변을 제외한 나머지 두 변의 길이 합이 가장 긴 변보다 크면 YES, 아니면 NO 출력

---
+++ section1/삼각형 판별하기.js
@@ -0,0 +1,25 @@
+/*
+ * 문제: 삼각형 판별하기
+ * [조건]
+ * - 길이가 서로 다른 A, B, C
+ * - 삼각형 만들 수 있으면 YES, 아니면 NO 출력
+ *
+ * [플로우]
+ * 1. 세 변의 길이 입력 받기
+ * 2. 가장 긴 변 찾기
+ * 3. 가장 긴 변을 제외한 나머지 두 변의 길이 합이 가장 긴 변보다 크면 YES, 아니면 NO 출력
+ * */
+
+function solution(a, b, c) {
+    let answer = "YES";
+    let max;
+    let sum = a + b + c;
+
+    if (a > b) max = a;
+    else max = b;
+    if (c > max) max = c;
+
+    if (sum - max <= max) answer = "NO";
+
+    return answer;
+}
