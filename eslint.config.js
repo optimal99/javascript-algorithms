f7906c3036df443e9ceac6d1365914a6307fb310
optimal99 <stegano9744@gmail.com>
2024-06-13 18:32:04 +0900

    Test: Jest 환경 구성

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,12 @@
-
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   eslintConfigPrettier,
   {
-    "rules": {
-      "prettier/prettier": "error",
-      "arrow-body-style": "off",
-      "prefer-arrow-callback": "off"
-    }
-  }
+    rules: {
+      'prettier/prettier': 'error',
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
 ];
