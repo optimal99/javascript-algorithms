ba99cf7f620026722ae2c929839872cc67af9772
optimal99 <stegano9744@gmail.com>
2024-06-13 19:00:53 +0900

    Docx: boolean.js 파일 JSDoc 작성

--- util/boolean.js
+++ util/boolean.js
@@ -1,3 +1,10 @@
+/*
+ * 입력한 숫자가 범위 내에 속하는지 확인하는 함수
+ * @param {number} numbers - 확인할 숫자(여러 개 가능)
+ * @param {number} min - 최솟값
+ * @param {number} max - 최댓값
+ * @returns {boolean} 모든 숫자가 범위 내에 속하면 true, 아니면 false를 반환합니다.
+ */
 export function areNumbersInRange(numbers, min, max) {
   return numbers.every(n => {
     return !(!Number.isInteger(n) || n < min || n > max);
