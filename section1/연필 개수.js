b39303920958b1fe2f9f7e19dad29ff15c9f2d64
optimal99 <stegano9744@gmail.com>
2024-06-13 09:05:07 +0900

    #1-3. 연필 개수
    [조건]
    - 연필 1다스는 12자루
    - 학생 1인당 연필 1자루씩 나눠줄 때 필요한 연필 다스 수 구하기
    
    [플로우]
    1. 학생 수 입력 받기
    2. 필요한 연필 다스 수 구하기
    3. 필요한 연필 다스 수 출력하기
    
    [함수]
    Math.ceil(): 올림
    Math.floor(): 내림
    Math.round(): 반올림

---
+++ section1/연필 개수.js
@@ -0,0 +1,27 @@
+/*
+ * 문제: 연필 개수
+ * [조건]
+ * - 연필 1다스는 12자루
+ * - 학생 1인당 연필 1자루씩 나눠줄 때 필요한 연필 다스 수 구하기
+ * - 1000명 이하의 학생 수 입력 받기
+ *
+ * [플로우]
+ * 1. 학생 수 입력 받기
+ * 2. 필요한 연필 다스 수 구하기
+ * 3. 필요한 연필 다스 수 출력하기
+ *
+ * [함수]
+ * Math.ceil(): 올림
+ * Math.floor(): 내림
+ * Math.round(): 반올림
+ * */
+
+import { areNumbersInRange } from '../util/boolean.js';
+
+function solution(...n) {
+  const isInRange = areNumbersInRange(n, 1, 1000);
+  return isInRange ? Math.ceil(n / 12) : false;
+}
+
+console.log(solution(25)); // 3
+console.log(solution(178)); // 15
