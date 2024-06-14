986566b1e820d501facf5bc91ba95d1ef10377b2
optimal99 <stegano9744@gmail.com>
2024-06-14 15:52:14 +0900

    Test: boolean.js 테스트 코드 추가 작성

--- util/test/boolean.test.js
+++ util/test/boolean.test.js
@@ -5,4 +5,9 @@ describe('boolean', () => {
     expect(areNumbersInRange(1, 1, 5)).toBe(true);
     expect(areNumbersInRange(1, 2, 5)).toBe(false);
   });
+
+  it('숫자 배열이 범위 내에 속하는지 확인하는 함수', () => {
+    expect(areNumbersInRange([1, 3, 5], 1, 5)).toBe(true);
+    expect(areNumbersInRange([1, 3, 6], 1, 5)).toBe(false);
+  });
 });
